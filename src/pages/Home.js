c0f5f0fa075fffa414daaefc00a02b3a0e23d8d7
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-01-29 17:24:47 +1100

    finall navbar and banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../component/HeroBanner'
+import SearchExercises from '../component/SearchExercises'
+import Exercises from '../component/Exercises'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
     )
 }
 
