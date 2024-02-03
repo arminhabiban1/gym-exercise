b9b2514c02bb50a49f7450f63a44d049b9d45b51
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-02-03 17:11:47 +1100

    add horizontalscrollbar

--- src/component/SearchExercises.js
+++ src/component/SearchExercises.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 const SearchExercises = () => {
     const [search, setSearch] = useState('')
     const [exercises, setExercises] = useState([])
     const [bodyParts, setBodyParts] = useState([])
     useEffect(() => {
         const fetchExercisesData = async () => {
-            const bodyPartData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList')
-            setBodyParts(['all', ...bodyPartData])
+            const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList')
+            setBodyParts(['all', ...bodyPartsData])
         }
         fetchExercisesData();
     }, [])
@@ -67,7 +68,7 @@ const SearchExercises = () => {
                     Search
                 </Button>
                 <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-                    {/* <HorizontalScrollbar data={bodyParts} /> */}
+                    <HorizontalScrollbar data={bodyParts} />
                 </Box>
             </Box>
 
