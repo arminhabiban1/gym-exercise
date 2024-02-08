7f03c5e74e5f122d9a25b79be696e9f08ba16e00
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-02-08 21:11:56 +1100

    add api to search

--- src/component/Exercises.js
+++ src/component/Exercises.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material'
+import { exerciseOptions, fetchData } from '../utils/fetchData'
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+    console.log(exercises)
     return (
-        <div>Exercises</div>
+        <Box id='exercises'
+            sx={{ mt: { lg: '110px' } }}
+            mt='50px'
+            p='20px'>
+            <Typography variant='h4' mb='46px'> Showing Results</Typography>
+            <Stack direction='row' sx={{ gap: { lg: '110px', xs: '50px' } }} flexWrap='wrap' justifyContent='center'>
+                {exercises.map((exercises, index) => (
+                    <p>{exercises.name}</p>
+                ))}
+            </Stack>
+        </Box>
     )
 }
 
